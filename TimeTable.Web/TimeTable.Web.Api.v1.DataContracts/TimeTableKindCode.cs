12d9835d9a1d0286642ffdbfbba1b8d0593ad867
Aleksandr <Alex-Gnatuyk@yandex.ru>
2017-11-01 12:02:35 +0300

    Add TimeTableKindCode into DataContracts

---
+++ TimeTable.Web/TimeTable.Web.Api.v1.DataContracts/TimeTableKindCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpbuEducation.TimeTable.Web.Api.v1.DataContracts
+{
+    public enum TimeTableKindСode
+        {		
+           /// <summary>		
+            /// Unknow timetable kind		
+            /// </summary>		
+            Unknown = 0,		
+            /// <summary>		
+            /// Primary timetable kind		
+            /// </summary>		
+            Primary = 1,
+            /// <summary>		
+            /// Interim (intermediary) attestation		
+            /// </summary>		
+            Attestation = 2,
+            /// <summary>		
+            /// Final attestation		
+            /// </summary>		
+            Final = 3		
+        }		
+}
